8abb15c6647f2aed6d244cec63b98d38b86a8bcd
andretdr <neckoid@hotmail.com>
2024-07-18 12:07:40 +0800

    adding onContextMenu={(e)=> e.preventDefault()} holdToDisplay={-1} to audio button

--- src/pages/AudioButton.jsx
+++ src/pages/AudioButton.jsx
@@ -53,7 +53,7 @@ const AudioSVG = (props) => {
     return (<div className='display-1 text-white'>
             STATUS = {status}
             TIME = {currTime}
-            <button id='audioButton' className='audio-button mb-5 bg-transparent border-0' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+            <button id='audioButton' className='audio-button mb-5 bg-transparent border-0' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onContextMenu={(e)=> e.preventDefault()} holdToDisplay={-1}>
                 <svg id='svg' className='svg svg-hover' fill="white" width="256px" height="256px" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003">
                     <g id="SVGRepo_bgCarrier" strokeWidth="0" />
                     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
